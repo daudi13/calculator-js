886d46f873c13af526ee48a54da359f1aa7e7af7
daudi13 <davouma13@gmail.com>
2021-04-15 04:08:51 +0300

    commit show number

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }
 
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
+    delete() {}
+    appendNumber(number) {
+        this.currentOperand = number;
+    }
+    chooseOperation(operation) {}
+    compute() { }
+    
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.currentOperand
+    }
+}
 const digitButtons = document.querySelectorAll('[data-number]');
 const actionButtons = document.querySelectorAll('[data-operation]');
 const deleteButton = document.querySelector('[data-delete]');
@@ -6,3 +29,12 @@ const allClearButton = document.querySelector('[data-all-clear]');
 const equalButton = document.querySelector('[data-equals]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+digitButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
