63a1b3ed336839b50c471d5c9d3aaec785c21b51
daudi13 <davouma13@gmail.com>
2021-04-15 05:28:35 +0300

    commit updates calc functions

--- app.js
+++ app.js
@@ -10,17 +10,63 @@ class Calculator {
         this.previousOperand = ''
         this.operation = undefined
     }
-    delete() {}
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+    
     appendNumber(number) {
         if(number === '.' && this.currentOperand.includes('.'))return
         this.currentOperand = this.currentOperand.toString() + number.toString();
     }
 
-    chooseOperation(operation) {}
-    compute() { }
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
+    
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '/':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
+
+    getDisplayNumber(number) {
+        const floatNumber = parseFloat(number)
+        if (isNaN(floatNumber)) return ''
+        return  floatNumber.toLocaleString('en')
+    }
     
     updateDisplay() {
-        this.currentOperandTextElement.innerText = this.currentOperand
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(previousOperand)} ${this.operation}`
+        }
     }
 }
 const digitButtons = document.querySelectorAll('[data-number]');
@@ -38,4 +84,27 @@ digitButtons.forEach(button => {
         calculator.appendNumber(button.innerText)
         calculator.updateDisplay()
     })
+})
+
+actionButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
 })
