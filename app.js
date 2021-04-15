aa00d5e4f9fdabee2c297cd557a660ff28546754
daudi13 <davouma13@gmail.com>
2021-04-15 03:23:34 +0300

    commit previous changes

---
+++ app.js
@@ -0,0 +1,8 @@
+
+const digitButtons = document.querySelectorAll('[data-number]');
+const actionButtons = document.querySelectorAll('[data-operation]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const equalButton = document.querySelector('[data-equals]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
